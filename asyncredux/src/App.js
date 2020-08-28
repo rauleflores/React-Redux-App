7a5fe7c905895d350678862661ddc69683c4286b
Raul Flores <raulefloresjr@gmail.com>
2020-08-27 18:06:58 -0700

    CRA done, provider&store set up

---
+++ asyncredux/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App(props) {
+	return (
+		<div className="App">
+			<h1>You son of a Rick, I'm in!</h1>
+		</div>
+	);
+}
+
+export default App;
